2b6c6fd97e4c555f8232b0c6671ad6c9f5a2c6ba
nbento <ratinhodaprogramacao@gmail.com>
2017-01-03 20:20:42 +0000

    Lec54 not important

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,12 @@ var About = React.createClass(
 });
 */
 var About = (props) => {
-	return (<h3>About 2017.01.02 BBB</h3>)
+	return (
+		<div>
+				<h1 className="text-center">About</h1>
+				<p>(Not Important...) Welcome to the About page!</p>
+
+		</div>)
 }
 
 module.exports = About;
